7d9ea99a078ac7afa94a804fa4cc250c8056f7eb
fault <critical@fault.io>
2015-12-03 16:32:57 -0700

    Initial commit describing project.

---
+++ project.py
@@ -0,0 +1,9 @@
+identity = 'https://fault.io/project/python/factor'
+name = 'factor'
+fork = 'root'
+release = None
+icon = '🦂'
+abstract = 'product structure interfaces for software projects'
+
+controller = 'fault.io' # Responsible Party
+contact = 'mailto:critical@fault.io' # Contact point for the Responsible Party
