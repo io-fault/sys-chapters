a9e2dc69002aa3ccd696327df4beaf66ff0b53a3
fault <critical@fault.io>
2015-12-30 14:36:54 -0700

    Implement a local function for discovering the real part of a reference.

--- xslt/__init__.py
+++ xslt/__init__.py
@@ -1,10 +1,17 @@
 __type__ = 'xsl'
 from ...xml import libfactor
+from ...routes import library as libroutes
 
-class Index(libfactor.XPathModule):
-	def test(self, context):
-		print('pass!!')
-		return ''
+class Factor(libfactor.XPathModule):
+	def reference(self, context, string, split=libroutes.Import.from_attributes):
+		"""
+		Return a node-set containing the real module path and the attributes following
+		the module.
+		"""
+		route, attributes = split(string)
+		module = str(route)
+		path = '.'.join(attributes)
+		return [module, path]
 
 libfactor.load()
 del libfactor
