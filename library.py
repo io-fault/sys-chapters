65f649291ede170688e23b7d41eb8e06738ff806
fault <critical@fault.io>
2016-06-22 16:34:51 -0700

    Initial support for composite factors.
    This with the prior commit is the initial support for composite factors
    that represent a collection of sources. The term 'fractions' is temporary.

--- library.py
+++ library.py
@@ -1,11 +1,15 @@
 """
+Factors library providing high level access to structuring and formatting the
+factors that make up a product.
 """
 import typing
+
 from ..routes import library as libroutes
 from ..filesystem import library as libfs
 from ..text import library as libtext
+from ..development import libfactor
 
-def factors(package:str, Route=libroutes.Import.from_fullname) -> typing.Tuple[
+def factors(package:str) -> typing.Tuple[
 		libroutes.Import,
 		typing.Sequence[libroutes.Import],
 		typing.Sequence[libroutes.Import]
@@ -14,11 +18,10 @@ def factors(package:str, Route=libroutes.Import.from_fullname) -> typing.Tuple[
 	Construct and return the factors (modules and packages) contained within
 	the given &package string.
 
-	[ Return ]
-
-		# root
-		# packages
-		# modules
+	[ Effects ]
+	/Product
+		`(root, (packages, modules))`; where root is the &package parameter
+		as an &libroutes.Import.
 
 	All objects are &..routes.library.Import instances pointing to the module.
 
@@ -27,6 +30,30 @@ def factors(package:str, Route=libroutes.Import.from_fullname) -> typing.Tuple[
 	/package
 		The path to the package.
 	"""
-
-	root = Route(package)
+	global libroutes
+	root = libroutes.Import.from_fullname(package)
 	return (root, root.tree())
+
+def fractions(packages:libroutes.Import) -> typing.Mapping[
+		libroutes.Import, typing.Sequence[libroutes.Import],
+	]:
+	"""
+	Construct a mapping detailing the factors that consist of a set of fractions.
+	Factors that represent the construction of a set of fractions are used to manage
+	build targets.
+
+	[Effects]
+	/Product
+		The constructed mapping. The keys are the &libroutes.Import instances,
+		and the values are sequences
+
+	[Parameters]
+	/packages
+		The set of factor packages to inspect in order to find the associated fractions.
+	"""
+	global libfactor
+	return {
+		x: libfactor.sources(x)
+		for x in packages
+		if libfactor.composite(x)
+	}
