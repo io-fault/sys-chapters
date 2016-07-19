ca228cbacc9f63e6f2efccda3ebf639cb4d0e731
fault <critical@fault.io>
2016-07-18 22:28:36 -0700

    Add executables section.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,12 @@
+"""
+[ Executables ]
+/&.bin.instantiate
+	Sturcture and Format a package into a target directory.
+
+/&.bin.structure
+	Structure a package into a target directory.
+
+/&.bin.format
+	Format a structured package into a set of HTML files.
+"""
 __factor_type__ = 'project'
