a74340ad8d3c9f178aacc38e6a01eaba812cc07b
fault <critical@fault.io>
2016-07-31 17:22:52 -0700

    Add status.

--- project.py
+++ project.py
@@ -4,6 +4,7 @@ abstract = 'Project documentation and release publishing tools.'
 icon = '🦂'
 
 fork = 'root'
+status = 'flux'
 
 controller = 'fault.io' # Responsible Party
 contact = 'mailto:critical@fault.io' # Contact point for the Responsible Party
