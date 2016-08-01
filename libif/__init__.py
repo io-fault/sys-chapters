72a5637b121d0ab113df7252f4e9e9d29c721fcb
fault <critical@fault.io>
2016-07-31 17:23:42 -0700

    Add factor types.

--- libif/__init__.py
+++ libif/__init__.py
@@ -3,3 +3,5 @@ Javascript Library supporting the HTML interface.
 """
 from ...web import libfactor
 libfactor.load('javascript')
+
+__factor_type__ = 'javascript.library'
--- theme/__init__.py
+++ theme/__init__.py
@@ -3,3 +3,5 @@ CSS for the HTML formatted documentation.
 """
 from ...web import libfactor
 libfactor.load('css')
+
+__factor_type__ = 'css.library'
