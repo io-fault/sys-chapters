c3564676efe78fb964b6c4a3ded1a7a3cdea1916
fault <critical@fault.io>
2017-05-24 00:40:30 -0700

    Duplicate some doc-string processing logic for relocation.

--- tools.py
+++ tools.py
@@ -1,11 +1,30 @@
 """
-Various utilities supporting processing.
+# Various utilities supporting processing.
 """
+
+def remove_common_indentation(lines):
+	"""
+	# Remove the leading indentation level from the given lines.
+	"""
+
+	# first non-empty line is used to identify
+	# the indentation level of the entire string.
+	for fl in lines:
+		if fl.strip():
+			break
+
+	if fl.startswith('\t'):
+		indentation = len(fl) - len(fl.lstrip('\t'))
+		return [x[indentation:] for x in lines]
+	else:
+		# presume no indentation and likely single line
+		return lines
 
 def strip_notation_prefix(lines, prefix='# '):
 	"""
-	# Remove the comment notation prefix from a string.
+	# Remove the comment notation prefix from a sequence of lines.
 	"""
+
 	pl = len(prefix)
 	return [
 		(('\t'*(xl-len(y))) + y[pl:] if y[:pl] == prefix else x)
@@ -14,3 +33,23 @@ def strip_notation_prefix(lines, prefix='# '):
 			for z in lines
 		]
 	]
+
+def normalize_documentation(lines, prefix='# '):
+	"""
+	# Remove the leading indentation level from the given lines.
+	"""
+
+	# first non-empty line is used to identify
+	# the indentation level of the entire string.
+	for fl in lines:
+		if fl.strip():
+			break
+
+	if fl.startswith('\t'):
+		indentation = len(fl) - len(fl.lstrip('\t'))
+		plines = strip_notation_prefix([x[indentation:] for x in lines], prefix=prefix)
+		return plines
+	else:
+		# assume no indentation and likely single line
+		plines = strip_notation_prefix(lines, prefix=prefix)
+		return plines
