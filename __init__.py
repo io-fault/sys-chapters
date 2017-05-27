c30ef1994a26a1de60076e573dbe36aceeb03ddc
fault <critical@fault.io>
2017-05-27 12:36:53 -0700

    Reduce package documentation.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,4 @@
 """
-# [ Executables ]
-# /&.bin.instantiate
-	# Sturcture and Format a package into a target directory.
-
-# /&.bin.structure
-	# Structure a package into a target directory.
-
-# /&.bin.format
-	# Format a structured package into a set of HTML files.
+# Factor inspection and reporting tools.
 """
 __factor_type__ = 'project'
