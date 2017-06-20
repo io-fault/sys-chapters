55a71a034d36eb0a7b1bde2b702254a5cbf5268e
fault <critical@fault.io>
2017-06-20 10:26:40 -0700

    Bitrot.

--- test/test_libhtml.py
+++ test/test_libhtml.py
@@ -1,21 +1,21 @@
 """
 # Check the XPath functions and some of the XSLT features.
 """
-from .. import xslt as library
+from .. import libhtml as module
 
 def test_Factor_duration(test):
-	f = library.Factor()
+	f = module.Factor()
 
 	test/f.duration(None, 6000) == '6µs'
 
 	# Hours are the largest unit in our reports.
-	day = library.libtime.Measure.of(day=1)
+	day = module.libtime.Measure.of(day=1)
 	test/f.duration(None, str(int(day))) == '24 hours'
 
-	c = library.libtime.Measure.of(day=1, minute=24, second=17, millisecond=403, microsecond=23)
+	c = module.libtime.Measure.of(day=1, minute=24, second=17, millisecond=403, microsecond=23)
 	test/f.duration(None, str(int(c))) == '24 hours 24 minutes 17s 403ms 23µs'
 
-	c = library.libtime.Measure.of(second=17, millisecond=403, microsecond=0, nanosecond=100)
+	c = module.libtime.Measure.of(second=17, millisecond=403, microsecond=0, nanosecond=100)
 	test/f.duration(None, str(int(c))) == '17s 403ms 100ns'
 
 if __name__ == '__main__':
