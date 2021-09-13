5f189bb67993b86116fa4f03f1955cdf11691295
fault <critical@fault.io>
2021-09-12 23:28:32 -0700

    Update variants usage.

--- bin/archive.py
+++ bin/archive.py
@@ -9,6 +9,7 @@ import zipfile
 import contextlib
 import json
 
+from fault.context import tools
 from fault.context.types import Cell
 from fault.system import files
 from fault.system import execution
@@ -20,7 +21,16 @@ from .. import join
 def apath(*parts):
 	return '/'.join(parts)
 
-def r_factor(archive, req, ctx, pj, pjdir, fpath, type, requirements, sources):
+@tools.cachedcalls(8)
+def mkvariants(intention, system, architecture):
+	return lsf.types.Variants(
+		system=system,
+		architecture=architecture,
+		intention=intention,
+		form='delineated',
+	)
+
+def r_factor(variants, archive, req, ctx, pj, pjdir, fpath, type, requirements, sources):
 	# project/factor
 	outset = apath(pjdir, str(fpath))
 
@@ -33,16 +43,17 @@ def r_factor(archive, req, ctx, pj, pjdir, fpath, type, requirements, sources):
 		ddepth = 1
 		meta_prefix = True
 		primary = ""
-	meta_json = json.dumps([meta_prefix, primary, type]).encode('utf-8')
+	meta_json = json.dumps([meta_prefix, primary, str(type)]).encode('utf-8')
 	archive.writestr(meta, meta_json)
 
-	vars = {
-		'intention': 'delineation',
-		'architecture': 'data',
-		'system': 'void',
-	}
+	for v in variants:
+		img = pj.image(v, fpath)
+		if img.fs_type() == 'directory':
+			break
+	else:
+		# No image.
+		return ""
 
-	img = pj.image(vars, fpath)
 	srcindex = []
 	for x in (x[1] for x in sources):
 		# Calculate path to delineation image.
@@ -72,14 +83,14 @@ def r_factor(archive, req, ctx, pj, pjdir, fpath, type, requirements, sources):
 	archive.writestr(apath(outset, '.index.json'), json.dumps(srcindex))
 	return ""
 
-def r_project(archive, req:lsf.Context, ctx:lsf.Context, pj:lsf.Project):
+def r_project(variants, archive, req:lsf.Context, ctx:lsf.Context, pj:lsf.Project):
 	# Currently hardcoded.
 	pjdir = str(pj.factor)
 	factors = []
 
 	for ((path, type), (reqs, sources)) in pj.select(lsf.types.factor):
-		summary = r_factor(archive, req, ctx, pj, pjdir, path, type, reqs, sources)
-		factors.append((str(path), type, summary))
+		summary = r_factor(variants, archive, req, ctx, pj, pjdir, path, type, reqs, sources)
+		factors.append((str(path), str(type), summary))
 
 	archive.writestr(apath(pjdir, '.index.json'), json.dumps(factors))
 
@@ -107,7 +118,7 @@ def first_sentence(p):
 	return ''.join(y[1] for y in p)
 
 def main(inv:process.Invocation) -> process.Exit:
-	outstr, ctxpath = inv.argv
+	outstr, ctxpath, *variant_s = inv.argv
 
 	# Build project context for the target product.
 	ctx = lsf.Context()
@@ -119,12 +130,18 @@ def main(inv:process.Invocation) -> process.Exit:
 	req = ctx.from_product_connections(pd)
 	req.load()
 
+	variants = [
+		mkvariants('coverage', x[0], x[1]) for x in (
+			y.split('/') for y in variant_s
+		)
+	]
+
 	out = files.Path.from_path(outstr)
 	arc = zipfile.ZipFile(str(out), mode='w', compression=zipfile.ZIP_DEFLATED, compresslevel=9)
 	with arc:
 		projects = []
 		for pj in ctx.iterprojects():
-			r_project(arc, req, ctx, pj)
+			r_project(variants, arc, req, ctx, pj)
 			projects.append(pj)
 
 		pjidx = [
